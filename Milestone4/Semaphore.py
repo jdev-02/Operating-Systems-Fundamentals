6a3db472e639a12ec6baabdb2af7096f2d0ea56c
raajpaalgohlwar <raajpaalgohlwar@gmail.com>
2025-09-04 21:46:48 -0700

    Worked on wait() and signal(). I believe I have a working implementation.

--- Milestone4/Semaphore.py
+++ Milestone4/Semaphore.py
@@ -22,7 +22,7 @@ class Semaphore(object):
         
         self.OS = simKernel
         self.counter = 1 # Assign counter initial value of 1 (lock is open)
-        self.userQueue = self.OS.getQueue() # Create queue
+        self.queue = [] # Create queue
         self.lock = self.OS.getAtomicLock() # Create lock
         
 ##########################################
@@ -33,52 +33,27 @@ class Semaphore(object):
             - caller is the process asking "wait?"
               (you will need caller because this is a simulated system,
                a production OS has this info available as part of the PCB)
-        
-        1. While semaphore != 1: loop/continue -> enter queue
-            2. Enter critical section/gain access to shared resource
-            3. Assign value of 0 to semaphore counter variable
-         
         '''
         if (self.counter == 1):
-            # Decrement counter
-            print("I am first inline")
-            #self.lock.acquire(caller)
+            # Lock is open
+            self.lock.acquire(caller)
             self.counter == 0
-            print(self.counter)
-            # Execute critical section
         elif (self.counter == 0):
             # Place waiting process in queue, go to sleep
-            print("I am going in the queue")
-            #self.counter -= 1
-            print(self.counter)
-            self.userQueue.put(caller)
+            self.queue.append(caller)
             caller.sleep()
-            print(self.userQueue)
             
     def signal(self, caller):
         ''' semaphore signal functionality.
             - caller is the process providing the "signal"
               (you will need caller because this is a simulated system,
                a production OS has this info available as part of the PCB)
-        
-        1. Assign value of 1 to semaphore counter variable
-        with lock
-            dequeue waiting user
-            increment counter back to 1
         '''
-        # Get next man up
-        print("I am now in signal")
-        if (self.userQueue.empty()):
-            self.counter = 1
+        # Release lock from current process
+        self.lock.release(caller)
+        if len(self.queue) > 0:
+            # Get next man up
+            nextMan = self.queue.pop(0)
+            self.OS.wake(nextMan)
         else:
-            self.OS.wake(self.userQueue.get())
-
-        '''
-        if (self.counter == 0):
-            #self.lock.release(caller)
-            self.counter += 1
-            print(self.counter)
-            self.OS.wake(self.userQueue.get())
-            #self.wait(nextman)
-            #now next man can enter since the counter is 1
-        '''
+            self.counter = 1
