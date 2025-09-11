13156a78cce0f6951446db4ce7d183e432deca20
Your Name <your-email@example.com>
2025-09-10 18:54:28 -0700

    a partially maybe potentially cool m4 code

--- Milestone4/Semaphore.py
+++ Milestone4/Semaphore.py
@@ -7,22 +7,23 @@ Updated: 13 Jan 2022
 '''
 
 '''This is the Semaphore implementation class.'''
+import multiprocessing as mp
 
 class Semaphore(object):
 ##########################################
 #Constructor
 
-    def __init__(self, counter, simKernel):
+    def __init__(self, n, simKernel):
         ''' use as provided.
             - n is the number to set the Semaphore counter to initially
             - simKernel provides the access to the simulated kernel a real OS
                  would have when constructing a Semaphore
             This constructor will run once when the OS invokes it.
         '''
-        
-        self.OS = simKernel
-        self.counter = 1 #Assign counter initial value of 1 (lock is open)
-        self.queue = [] #create abstract queue
+
+        self.OS = simKernel        
+        self.counter = mp.Value('i',n).value
+        self.queue = self.OS.getQueue() #create abstract queue
         self.lock = self.OS.getAtomicLock() #create lock
         
 ##########################################
@@ -32,17 +33,22 @@ class Semaphore(object):
         ''' semaphore wait functionality.
             - caller is the process asking "wait?"
               (you will need caller because this is a simulated system,
-               a production OS has this info available as part of the PCB)
+               a production OS has this info available as part of the PCB)                 
+               
         '''
-        if (self.counter == 1):
+        self.lock.acquire(caller)
+        if (self.counter > 0):
             #lock is open
-            self.lock.acquire(caller)
-            #caller gets atom lock
-            self.counter = 0
+            self.counter -= 1
+            self.lock.release(caller)
         elif (self.counter == 0):
             #place waiting process in queue bc resource is locked, go to sleep
-            self.queue.append(caller)
+            self.queue.put(caller)
+            self.lock.release(caller)
             caller.sleep()
+        else:
+            self.lock.release(caller)
+
             
     def signal(self, caller):
         ''' semaphore signal functionality.
@@ -51,13 +57,16 @@ class Semaphore(object):
                a production OS has this info available as part of the PCB)
         '''
         #release lock from current process
-        self.lock.release(caller)
-        if len(self.queue) > 0:
+        self.lock.acquire(caller)
+        if self.queue.empty() != True:
             #get next man up
-            nextMan = self.queue.pop(0)
+            nextMan = self.queue.get()
             self.OS.wake(nextMan)
-        else:
-            self.counter = 1 
+            self.counter += 1
+            self.lock.release(caller)
+        else: #queue empty
+            self.counter += 1 
+            self.lock.release(caller)
             #resource is free to be grabbed by an incoming process since nothing is in the queue waiting
 
 #EOF
