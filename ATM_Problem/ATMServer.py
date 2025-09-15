d243f72f83884148f8a676e6e3dcf2d4feceba4c
Tamarcus Norman <tamarcusn2@gmail.com>
2025-09-14 21:39:38 -0700

    Norman Version

--- ATM_Problem/ATMServer.py
+++ ATM_Problem/ATMServer.py
@@ -1,11 +1,10 @@
 #
 # ATMServer.py
-# CS3070 Synchronization Lab series 
+# CS3070 Synchronization Lab series
 #
 # created: Spring '16
-# updated: 13 Jan 2022
+# updated: 13 Jan 2022 (+ M5 fixes: semaphore + delta application)
 #
-
 
 import random
 import time
@@ -15,8 +14,6 @@ import SL_Kernel as Kernel
 from Semaphore import *
 from ATMMessage import *
 
-
-
 '''This is bank's ATM server class.
 There are multiples of these running at the bank which may access the same accounts at the same time. It
 does not matter if we think of them running on the same huge machine or many little ones, the data is shared.
@@ -25,7 +22,6 @@ It does not matter if the data is shared inside a database, a data structure, in
 RAM, or in a sandard RAM.  Shared is shared and essentially the only thing that changes is timing.
 '''
 class ATMServer(mp.Process):
-
 
 ##########################################
 #Constructor
@@ -45,13 +41,9 @@ class ATMServer(mp.Process):
         self.__TRANSACTION_LIMIT__ = transactionLimit
         self.again = True
         self.seed = seed
-        
-
-        
 
 ##########################################
 #Instance Methods
-
 
     def execute(self, q, al, ml, n):
         '''this is the function which serves as the processes "Run loop" '''
@@ -59,43 +51,79 @@ class ATMServer(mp.Process):
         self.__initializeSimComponents(q, al)
         self.semaphore = Semaphore(n, self.OS)
 
+        # This server talks to exactly one ATM over self.atm_connection.
+        # Track the last balance we returned to THIS client so we can derive deltas on PUT.
+        last_read = None
+
         i = 0
         while self.again:
 
-            #check and act on sim's stop condition 
+            #check and act on sim's stop condition
             if i >= self.__TRANSACTION_LIMIT__:
                 self.again = False
                 break
 
             i += 1
-            
+
             #waiting for next message
             message = self.atm_connection.recv()
             (operation, amount) = ATMMessage.unwrap(message)
 
             if operation == PUT_BALANCE:
-                self.putBalance(amount)
-                
+                # Client sends an absolute new balance it computed from its last read.
+                if last_read is None:
+                    # In this lab, ATM always does GET before any PUT, but guard just in case.
+                    # Treat this as a delta from current state if GET never happened.
+                    self.semaphore.wait(self)
+                    try:
+                        cur = self.getBalance()
+                        new_balance = amount  # absolute write fallback (shouldn't happen in normal flow)
+                        # Prefer delta behavior even here to avoid clobber: apply (amount - cur)
+                        delta = amount - cur
+                        new_balance = cur + delta
+                        self.putBalance(new_balance)
+                        last_read = new_balance
+                    finally:
+                        self.semaphore.signal(self)
+                else:
+                    # Normal path: derive the intended change and apply it atomically to CURRENT state
+                    delta = amount - last_read
+
+                    self.semaphore.wait(self)
+                    try:
+                        cur = self.getBalance()
+                        new_balance = cur + delta
+                        self.putBalance(new_balance)
+                        # Keep our notion in sync with what the client would see next
+                        last_read = new_balance
+                    finally:
+                        self.semaphore.signal(self)
+
             elif operation == GET_BALANCE:
-                amount = self.getBalance()
-                msg = ATMMessage.wrap(BALANCE, amount)
+                # Read the current shared state under the semaphore
+                self.semaphore.wait(self)
+                try:
+                    cur = self.getBalance()
+                    last_read = cur
+                finally:
+                    self.semaphore.signal(self)
+
+                # Send the reply outside the critical section
+                msg = ATMMessage.wrap(BALANCE, cur)
                 self.atm_connection.send(msg)
-                
+
             else:
                 raise RuntimeError(operation + 'is an unrecognized account operation')
-       
+
         self.atm_connection.send(SHUTDOWN)   #tell ATM to shutdown
         print('   ATMServer', self.clientName, 'shut down')
         self.OS.completeShutDown()
 
-
     def getBalance(self):
         return self.OS.read(self.account)
 
-
     def putBalance(self, newBalance):
         self.OS.write(self.account, newBalance)
-
 
     def __initializeSimComponents(self, q, al):
         self.OS.SIM_SETUP_setUpKernel(q, al, self.name)
@@ -105,11 +133,9 @@ class ATMServer(mp.Process):
         self.OS.al = self.OS.Lock(self.OS)
         self.OS.read(self.account)                    #verify we are connected to proper memory
 
-
     def getName(self):
         ''' returns the string that is the name of thie UserProcess'''
         return self.name
-
 
     def start(self):
         ''' This is how we get Python to run our custom code in a process.
@@ -122,29 +148,20 @@ class ATMServer(mp.Process):
         '''
         super().start()
 
-
     def join(self):
         ''' Send a message to parent calling this when complete, parent will wait until all joining
             children have completed before progressing past join barrier. '''
-        super().join() 
-
-
+        super().join()
 
     def sleep(self):
         '''puts thread into wait state'''
         #print('      calling for sleep (via wait) on', self.name)
         self.flag.wait()
 
-
     def wake(self):
         '''puts thread back into running state '''
         self.flag.set()   #wake the process' thread up
 
-
     def slp_yield(self):
         '''tells scheduler to take thread off CPU and put into ready queue'''
         self.flag.wait(0.0001)
-        
-
-
-
